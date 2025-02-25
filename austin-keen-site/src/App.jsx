ee5619d9ddc5c6f88bc79db7856cfdd0fe4d738f
Austin Keen <austinkeen1997@gmail.com>
2025-02-24 18:10:53 -0600

    add rest of boilerplate

--- austin-keen-site/src/App.jsx
+++ austin-keen-site/src/App.jsx
@@ -1,10 +1,20 @@
 import styles from "./App.module.css";
 import { NavBar } from "./components/NavBar/NavBar";
+import { Hero } from "./components/Hero/Hero";
+import { About } from "./components/About/About";
+import { Experience } from "./components/Experience/Experience";
+import { Contact } from "./components/Contact/Contact";
+import { Projects } from "./components/Projects/Projects";
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
+      <Hero />
+      <About />
+      <Experience />
+      <Projects />
+      <Contact />
     </div>
   );
 }
